07e6ed1fc0824f485c9d1871e548d21d59d5281b
Sylvain Beaubois <sylvain_beaubois@hotmail.fr>
2018-03-02 08:17:45 +0100

    route réglée

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'emails/index'
+
+	root to: 'emails#index'
+
+  	get 'emails/index', to: 'emails#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
